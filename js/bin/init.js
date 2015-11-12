4b09eb0267d9808a940fe30b7cdb491e9618f291
Seamus Leonard <seamus@steadynow.com>
2015-11-12 09:47:44 -0800

    added date picker

--- js/bin/init.js
+++ js/bin/init.js
@@ -6,6 +6,11 @@
     $('.scrollspy').scrollSpy();
     $('.modal-trigger').leanModal();
     $('ul.tabs').tabs();
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15 // Creates a dropdown of 15 years to control year
+      });
+            
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
